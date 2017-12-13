7a8114391eea3471ea274d53dbc347fee51ea072
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-12-13 22:53:48 +0800

    check更换新方法

--- check.py
+++ check.py
@@ -1,25 +1,38 @@
-def dynamic(x):
-    """使用动态规划的方法找到拆分数，速度快，占用空间大"""
-    num = x
-    table = [[0 for m in range(num)] for n in range(num)] # 构建动态规划表格
-    for n in range(num):
-        for m in range(num):
-            if n is 0 or m is 0:
-                table[n][m] = 1
-            elif n <= m:
-                table[n][m] = table[n][n-1]+1
-            else:
-                table[n][m] = table[n][m-1]+table[n-m-1][m]
-    return table[-1][-1]
+def pentagonal_number(x):
+        """使用五边形数定理计算拆分数，速度快，占用空间小"""
+        num = x
+        # 构建辅助数组
+        assist = []
+        for i in range(1, num):
+            assist.append(int(i*(i*3-1)/2))
+            assist.append(int(i*(i*3+1)/2))
+
+        # 构建由1~num的拆分数列表
+        p_list = [1, 1, 2]
+        for i in range(3, num+1):
+            count = 0
+            p = 0
+            for j in range(0, i):
+                if assist[j] > i:
+                    break
+                count %= 4
+                if count is 0 or count is 1:
+                    p += p_list[i-assist[j]]
+                else:
+                    p -= p_list[i-assist[j]]
+                count += 1
+            p_list.append(p)
+        
+        return p_list[num]
 
 
 def main():
     for m in range(0, 101):
-        p = dynamic(5*m+4)
+        p = pentagonal_number(5*m+4)
         assert p % 5 == 0, '5*{}+4 failed!'.format(m)
-        p = dynamic(7*m+5)
+        p = pentagonal_number(7*m+5)
         assert p % 7 == 0, '7*{}+5 failed!'.format(m)
-        p = dynamic(11*m+6)
+        p = pentagonal_number(11*m+6)
         assert p % 11 == 0, '11*{}+6 failed!'.format(m)
 
         print('m={} checked'.format(m))
